dfce9e0ce1320e9e6ba23c6c95ef73f16a120ac0
komalankolekar <75617813+komalankolekar@users.noreply.github.com>
2023-07-04 17:00:33 +0530

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,4 +1,9 @@
 def call(username){
   echo "Hi ${username}"
-     
+   if (${username} != admin){
+    echo "This is not admin user and username is $username"
+  }
+  else{
+    echo "This is admin user"
+  }  
 }
