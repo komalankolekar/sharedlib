99aeb888ccdd06d243fcef6539afc12623b590cf
komalankolekar <75617813+komalankolekar@users.noreply.github.com>
2023-07-05 11:32:58 +0530

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,12 +1,13 @@
+
 def call(username){
   echo "Hi ${username}"
    if ("${username}" != "[admin]"){
     echo "This is not admin user and username is $username"
    
-  currentBuild.result = 'ABORTED'
+  currentBuild.result = 'FAILURE'
      echo "${currentBuild.result}"
   return
-     echo "after fail"
+     //echo "after fail"
   }
   else{
     echo "This is admin user"
