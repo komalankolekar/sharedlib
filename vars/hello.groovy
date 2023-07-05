2c6b50852d047681c32279a428fc06b4fce92676
komalankolekar <75617813+komalankolekar@users.noreply.github.com>
2023-07-05 08:37:47 +0530

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -2,8 +2,10 @@ def call(username){
   echo "Hi ${username}"
    if ("${username}" != "[admin]"){
     echo "This is not admin user and username is $username"
-     currentBuild.build().getExecutor().interrupt(Result.FAILURE)
-     return
+     //currentBuild.build().getExecutor().interrupt(Result.FAILURE)
+     //return
+     //def build = Jenkins.instance.getItemByFullName("jobName").getBuildByNumber(jobNumber)
+     currentBuild.build().getExecutor().doKill()
      echo "after fail"
   }
   else{
