fdec7967dc558d0a6f7328a487e0e130338c3fc1
komalankolekar <75617813+komalankolekar@users.noreply.github.com>
2023-07-05 09:28:41 +0530

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -2,12 +2,7 @@ def call(username){
   echo "Hi ${username}"
    if ("${username}" != "[admin]"){
     echo "This is not admin user and username is $username"
-     //currentBuild.build().getExecutor().interrupt(Result.FAILURE)
-     //return
    
-               //sh "exit 1"
-                        //or
-                //error "Failed, exiting now..."
   currentBuild.result = 'ABORTED'
      echo "${currentBuild.result}"
   return
