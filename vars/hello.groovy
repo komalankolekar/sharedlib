92d1cbf2945babdaf89821403d31e4c6cc682270
komalankolekar <75617813+komalankolekar@users.noreply.github.com>
2023-07-05 12:37:48 +0530

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -4,10 +4,10 @@ def call(username){
    if ("${username}" != "[admin]"){
     echo "This is not admin user and username is $username"
      currentBuild.result = 'FAILURE'
-     error "Can not execute if branch is other than Dev!"
+     error "${username} Can not execute if branch is other than Dev!"
   return
     
-     //echo "after fail"
+
   }
   else{
     echo "This is admin user"
