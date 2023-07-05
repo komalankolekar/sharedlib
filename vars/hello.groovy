e54eb4127cef7f0d697caef992d94c9b3ae0c1d2
komalankolekar <75617813+komalankolekar@users.noreply.github.com>
2023-07-05 12:11:53 +0530

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -3,11 +3,8 @@ def call(username){
   echo "Hi ${username}"
    if ("${username}" != "[admin]"){
     echo "This is not admin user and username is $username"
-     t = currentBuild.number
-    echo "${t}"
-  currentBuild.result = 'FAILURE'
-     error "This pipeline stops here!"
-  // sh 'exit 1'
+     currentBuild.result = 'FAILURE'
+     //error "This pipeline stops here!"
   return
     
      //echo "after fail"
